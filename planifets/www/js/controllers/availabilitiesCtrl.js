0cd957575b9ca9f9d7c45f0630a9943353187b2e
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-03 16:28:52 -0400

    - Modified selector to pass selected value when clicking the view availabilities button - Changed header name for availabilities.html

--- planifets/www/js/controllers/availabilitiesCtrl.js
+++ planifets/www/js/controllers/availabilitiesCtrl.js
@@ -1,3 +1,40 @@
-angular.module('planifETS.controllers').controller('AvailabilitiesCtrl', ['$scope', '$http', function($scope, $http){
-  
-}]);
+angular.module('planifETS.controllers').controller('AvailabilitiesCtrl', function($scope, $http, $ionicLoading, $location){
+
+  $scope.selectedField = {};
+
+  $scope.viewAvailabilities = function() {
+    // Start showing the progress
+    $scope.show($ionicLoading);
+    // Call the service to get the data
+
+    $scope.hide($ionicLoading);
+
+    console.log($scope.selectedField.value);
+
+    $location.path('app/availability');
+  };
+
+  $scope.fields = [
+    { title: 'Automated manufacturing engineering', id: 1 },
+    { title: 'Construction engineering', id: 2 },
+    { title: 'Electrical engineering', id: 3 },
+    { title: 'Information technology engineering', id: 4 },
+    { title: 'Mechanical engineering', id: 5 },
+    { title: 'Operations and logistics engineering', id: 6 },
+    { title: 'Software engineering', id: 7 }
+  ];
+
+  // Setting the default value for the selector
+  $scope.selectedField.value = $scope.fields[0];
+
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner>'
+    });
+  };
+
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+
+});
