5b3fdc893fbd8c1e41f1efc126c02e1d000c7446
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-03 22:36:38 -0400

    Added confirmation dialog to delete button when deleting a planning

--- planifets/www/js/controllers/planningsCtrl.js
+++ planifets/www/js/controllers/planningsCtrl.js
@@ -1,4 +1,4 @@
-angular.module('planifETS.controllers').controller('PlanningsCtrl', function($scope, $ionicListDelegate){
+angular.module('planifETS.controllers').controller('PlanningsCtrl', function($scope, $ionicListDelegate, $ionicPopup){
 
   $scope.listCanSwipe = true;
   $scope.showDelete = false;
@@ -26,12 +26,28 @@ angular.module('planifETS.controllers').controller('PlanningsCtrl', function($sc
     $scope.plannings.splice(toIndex, 0, planning);
   };
 
+  // A confirm dialog
+  $scope.showConfirm = function(planning) {
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Confirmation',
+      template: 'Are you sure you want to delete "' + planning.title + '" ?'
+    });
+
+    confirmPopup.then(function(res) {
+      if(res) {
+        $scope.onItemDelete(planning)
+      } else {
+        $ionicListDelegate.closeOptionButtons();  // this closes swipe option buttons after alert
+      }
+    });
+  };
+
   $scope.onItemDelete = function(planning) {
     $scope.plannings.splice($scope.plannings.indexOf(planning), 1);
   };
 
   $scope.createPlanning = function() {
-    
+
   };
 
   $scope.plannings = [
