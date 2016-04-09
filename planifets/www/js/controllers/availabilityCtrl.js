eaf6eecfa3f663d224b13a623323d25e0a25a965
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-09 10:32:47 -0400

    Added arrays with data to fill in the view

--- planifets/www/js/controllers/availabilityCtrl.js
+++ planifets/www/js/controllers/availabilityCtrl.js
@@ -1,3 +1,28 @@
-angular.module('planifETS.controllers').controller('AvailabilityCtrl', ['$scope', '$http', function($scope, $http){
-  
-}]);
+angular.module('planifETS.controllers').controller('AvailabilityCtrl', function($scope, TitleService){
+
+  $scope.programTitle = null;
+
+  $scope.semesters = [
+    { title: 'W16', id: 1 },
+    { title: 'S16', id: 2 },
+    { title: 'A16', id: 3 },
+    { title: 'W17', id: 4 },
+    { title: 'S17', id: 5 }
+  ];
+
+  $scope.courses = [
+    { title: 'ING160', id: 1 },
+    { title: 'ING150', id: 2 },
+    { title: 'TIN501', id: 3 },
+    { title: 'GTI610', id: 4 },
+    { title: 'GTI510', id: 5 }
+  ];
+
+  $scope.downloadPDF = function() {
+
+  };
+  $scope.$on('$ionicView.beforeEnter', function() {
+    $scope.programTitle = TitleService.getTitle();
+  });
+
+});
