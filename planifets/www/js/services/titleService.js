83ad8657087972fe159ba0a3011c96713165e226
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-09 10:22:27 -0400

    Added titleService.js

---
+++ planifets/www/js/services/titleService.js
@@ -0,0 +1,17 @@
+angular.module('planifETS.controllers').factory('TitleService', function() {
+
+  var pageTitle = null;
+
+  var setTitle = function(title) {
+    pageTitle = title;
+  };
+
+  var getTitle = function() {
+    return pageTitle;
+  };
+
+  return {
+    getTitle : getTitle,
+    setTitle : setTitle
+  };
+})
