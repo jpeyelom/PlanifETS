adb3c34631cccda61a5c1d89fb65bed12691e372
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-09 10:22:57 -0400

    Added courseService.js

---
+++ planifets/www/js/services/courseService.js
@@ -0,0 +1,17 @@
+angular.module('planifETS.controllers').factory('CourseService', function() {
+
+  var selectedCourse = null;
+
+  var setSelectedCourse = function(course) {
+    selectedCourse = course;
+  };
+
+  var getSelectedCourse = function() {
+    return selectedCourse;
+  };
+
+  return {
+    getSelectedCourse : getSelectedCourse,
+    setSelectedCourse : setSelectedCourse
+  };
+})
