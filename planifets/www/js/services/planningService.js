cb7020577f2e694e5ae4cb25b765cd3cabe83ad4
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-09 10:22:42 -0400

    Added planningService.js

---
+++ planifets/www/js/services/planningService.js
@@ -0,0 +1,40 @@
+angular.module('planifETS.controllers').factory('PlanningService', function() {
+
+  var planningList = null;
+  var coursesList = null;
+  var planningListStore = localStorage.getItem("planningList");
+  var coursesListStore = localStorage.getItem("coursesList");
+  if (planningListStore != null && planningListStore != '' && angular.isArray(angular.fromJson(planningListStore))) {
+    planningList = angular.fromJson(planningListStore);
+  }
+  if (coursesListStore != null && coursesListStore != '' && angular.isArray(angular.fromJson(coursesListStore))) {
+    coursesList = angular.fromJson(coursesListStore);
+  }
+  var listSrv = {
+    setPlanningList: function(newList) {
+      planningList = newList;
+      localStorage.setItem("planningList", angular.toJson(planningList));
+      return true;
+    },
+    getPlanningList: function() {
+      if (planningList != null) {
+        return planningList;
+      } else {
+        return [];
+      }
+    },
+    setCoursesList: function(newList) {
+      coursesList = newList;
+      localStorage.setItem("coursesList", angular.toJson(coursesList));
+      return true;
+    },
+    getCoursesList: function() {
+      if (coursesList != null) {
+        return coursesList;
+      } else {
+        return [];
+      }
+    }
+  };
+  return listSrv;
+});
