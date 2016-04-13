181793bc0e623413acded3806aa730926e2e84df
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-13 04:17:07 -0400

    Added getter and setter for planningId and currentPlanning in planningService

--- planifets/www/js/services/planningService.js
+++ planifets/www/js/services/planningService.js
@@ -2,6 +2,7 @@ angular.module('planifETS.controllers').factory('PlanningService', [ function() 
 
   var planningList = null;
   var coursesList = null;
+  var currentPlanningId = localStorage.getItem("currentPlanningId");;
   var planningListStore = localStorage.getItem("planningList");
   var coursesListStore = localStorage.getItem("coursesList");
   if (planningListStore != null && planningListStore != '' && angular.isArray(angular.fromJson(planningListStore))) {
@@ -34,6 +35,25 @@ angular.module('planifETS.controllers').factory('PlanningService', [ function() 
       } else {
         return [];
       }
+    },
+    setCurrentPlanningId: function(id) {
+      currentPlanningId = id;
+      localStorage.setItem("currentPlanningId", angular.toJson(currentPlanningId));
+      return true;
+    },
+    getCurrentPlanningId: function() {
+      return currentPlanningId;
+    },
+    getCurrentPlanning: function() {
+      var planning = null;
+
+      for(var i = 0; 0 < planningList.length; i++) {
+        if(currentPlanningId == planningList[i].id) {
+          planning = planningList[i];
+          break;
+        }
+      }
+      return planning;
     }
   };
   return listSrv;
