46daf955481ee4cb9388c18edf0d914c2d12820f
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-13 04:18:32 -0400

    Removed static variables and added verifyAvailabilities(), showAvailabilities(), isCourseAvailable() functions in planningCtrl.js

--- planifets/www/js/controllers/planningCtrl.js
+++ planifets/www/js/controllers/planningCtrl.js
@@ -1,17 +1,84 @@
-angular.module('planifETS.controllers').controller('PlanningCtrl',['$scope', 'TitleService', 'PlanningService',
-  function($scope, TitleService, PlanningService){
+angular.module('planifETS.controllers').controller('PlanningCtrl',
+  ['$scope', '$ionicPopup', 'TitleService', 'PlanningService',
+    'AvailabilityService', 'CourseService', '$ionicLoading', 'SemesterService',
+  function($scope, $ionicPopup, TitleService, PlanningService
+    , AvailabilityService, CourseService, $ionicLoading, SemesterService){
 
-  $scope.navTitle = null;
-  $scope.showValid = false;
-  $scope.showInvalid = false;
-  $scope.available = [];
-  $scope.planningCourses = PlanningService.getCoursesList();
+    $scope.navTitle = null;
+    $scope.available = [];
+    $scope.planningCourses = PlanningService.getCoursesList();
+    $scope.plannings = PlanningService.getPlanningList();
+    $scope.currentPlanning = null;
+    $scope.currentSemester = null;
+    $scope.coursesAvailabilitiesFiltered = [];
 
-  $scope.verifyAvailabilities = function() {
-    $scope.showValid = ! $scope.showValid;
-  };
+    $scope.verifyAvailabilities = function() {
+      // Start showing the progress
+      $scope.show($ionicLoading);
 
-  $scope.$on('$ionicView.beforeEnter', function() {
-    $scope.navTitle = TitleService.getTitle();
-  });
+      var course = null;
+      var courseId = null;
+
+      $scope.currentPlanning = PlanningService.getCurrentPlanning();
+      var semester = $scope.currentPlanning.semester;
+      var semesterId = SemesterService.getSemesterId(semester);
+      SemesterService.setCurrentSemester(semester);
+      $scope.currentSemester = semester;
+
+      for (var i = 0; i < $scope.planningCourses.length; i++) {
+        course = $scope.planningCourses[i].course;
+        courseId = CourseService.getCourseId(course);
+
+        $scope.coursesAvailabilitiesFiltered = [];
+        $scope.coursesAvailabilitiesFiltered = AvailabilityService.filterCourseAvailabilities(courseId);
+
+        $scope.planningCourses[i].status = $scope.isCourseAvailable(semesterId, $scope.coursesAvailabilitiesFiltered);
+      }
+
+      $scope.hide($ionicLoading);
+    };
+
+    $scope.isCourseAvailable = function(currentSemesterId, coursesAvailabilitiesFiltered) {
+        for (var j = 0; j < coursesAvailabilitiesFiltered.length; j++) {
+          if (currentSemesterId == coursesAvailabilitiesFiltered[j].semester) {
+            if (coursesAvailabilitiesFiltered[j].availability == 1 ||
+              coursesAvailabilitiesFiltered[j].availability == 2 ||
+              coursesAvailabilitiesFiltered[j].availability == 3) {
+             return 'available';
+            } else {
+              return 'unavailable';
+            }
+          }
+        }
+    };
+
+    $scope.showAvailabilities = function(courses) {
+      if(courses.status == 'unavailable'){
+        $scope.showAvailabilityAlert('Course not available for ' + $scope.currentSemester,
+          courses.course + ' is only available for the current ' +
+          'semesters : <br/> W16 - D, S16 - N, W17 - D, S17 - N');
+      }
+    };
+
+    $scope.$on('$ionicView.beforeEnter', function() {
+      $scope.navTitle = TitleService.getTitle();
+    });
+
+    // An alert dialog
+    $scope.showAvailabilityAlert = function(title, template) {
+      var alertPopup = $ionicPopup.alert({
+        title: title,
+        template: template
+      });
+    };
+
+    $scope.show = function() {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>'
+      });
+    };
+
+    $scope.hide = function(){
+      $ionicLoading.hide();
+    };
 }]);
