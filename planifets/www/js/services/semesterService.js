38ea06501fe4ef8a3cb5e1ea7b77d2b6637adb4d
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-13 04:16:41 -0400

    Added getter and setter for semester and semesterId in semesterService

--- planifets/www/js/services/semesterService.js
+++ planifets/www/js/services/semesterService.js
@@ -2,6 +2,7 @@ angular.module('planifETS.controllers').factory('SemesterService',[ function() {
 
   var selectedSemester = null;
   var allSemesters = [];
+  var currentSemester = null;
 
   var setSelectedSemester = function(semester) {
     selectedSemester = semester;
@@ -19,10 +20,34 @@ angular.module('planifETS.controllers').factory('SemesterService',[ function() {
     allSemesters = array;
   };
 
+  var setCurrentSemester = function(semester) {
+    currentSemester = semester;
+  };
+
+  var getCurrentSemester = function() {
+    return currentSemester;
+  };
+
+  var getSemesterId = function(title) {
+    var semesterId = null;
+
+    for(var i = 0; 0 < allSemesters.length; i++) {
+      if(title == allSemesters[i].semester) {
+        semesterId = allSemesters[i].id;
+        break;
+      }
+    }
+
+    return semesterId;
+  };
+
   return {
     getSelectedSemester : getSelectedSemester,
     setSelectedSemester : setSelectedSemester,
     getAllSemesters : getAllSemesters,
-    setAllSemesters : setAllSemesters
+    setAllSemesters : setAllSemesters,
+    getSemesterId : getSemesterId,
+    setCurrentSemester : setCurrentSemester,
+    getCurrentSemester : getCurrentSemester
   };
 }]);
