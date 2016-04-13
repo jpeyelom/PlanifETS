098ca197b501778bf46606451f2e69fec3636178
Jean-Philippe Eyelom <jeanphilippe.eyelom@gmail.com>
2016-04-13 05:03:33 -0400

    Adding unfinished function to give other semester availabilities for a course that is not available in a specific semester.

--- planifets/www/js/controllers/planningCtrl.js
+++ planifets/www/js/controllers/planningCtrl.js
@@ -10,7 +10,14 @@ angular.module('planifETS.controllers').controller('PlanningCtrl',
     $scope.plannings = PlanningService.getPlanningList();
     $scope.currentPlanning = null;
     $scope.currentSemester = null;
+    $scope.currentSemesterId = null;
     $scope.coursesAvailabilitiesFiltered = [];
+    $scope.availabilitiesBySemesters = [];
+    $scope.availability = [
+      {title: 'D', id: 1},
+      {title: 'N', id: 2},
+      {title: 'DN', id: 3}
+    ];
 
     $scope.verifyAvailabilities = function() {
       // Start showing the progress
@@ -24,6 +31,7 @@ angular.module('planifETS.controllers').controller('PlanningCtrl',
       var semesterId = SemesterService.getSemesterId(semester);
       SemesterService.setCurrentSemester(semester);
       $scope.currentSemester = semester;
+      $scope.currentSemesterId = semesterId;
 
       for (var i = 0; i < $scope.planningCourses.length; i++) {
         course = $scope.planningCourses[i].course;
@@ -39,24 +47,46 @@ angular.module('planifETS.controllers').controller('PlanningCtrl',
     };
 
     $scope.isCourseAvailable = function(currentSemesterId, coursesAvailabilitiesFiltered) {
-        for (var j = 0; j < coursesAvailabilitiesFiltered.length; j++) {
-          if (currentSemesterId == coursesAvailabilitiesFiltered[j].semester) {
-            if (coursesAvailabilitiesFiltered[j].availability == 1 ||
-              coursesAvailabilitiesFiltered[j].availability == 2 ||
-              coursesAvailabilitiesFiltered[j].availability == 3) {
-             return 'available';
-            } else {
+      for (var j = 0; j < coursesAvailabilitiesFiltered.length; j++) {
+        if (currentSemesterId == coursesAvailabilitiesFiltered[j].semester) {
+          if (coursesAvailabilitiesFiltered[j].availability == 1 ||
+            coursesAvailabilitiesFiltered[j].availability == 2 ||
+            coursesAvailabilitiesFiltered[j].availability == 3) {
+              return 'available';
+          } else {
               return 'unavailable';
-            }
           }
         }
+      }
     };
 
     $scope.showAvailabilities = function(courses) {
+      var availabilities = [];
+      var item = {
+        value: "",
+        semester: ""
+      };
+      var courseId = CourseService.getCourseId(courses.course);
+      var coursesAvailabilitiesFiltered = AvailabilityService.filterCourseAvailabilities(courseId);
+
+      for (var k = 0; k < coursesAvailabilitiesFiltered.length; k++) {
+        if ($scope.currentSemesterId != coursesAvailabilitiesFiltered[k].semester) {
+          if (coursesAvailabilitiesFiltered[k].availability == 1 ||
+            coursesAvailabilitiesFiltered[k].availability == 2 ||
+            coursesAvailabilitiesFiltered[k].availability == 3) {
+              item.value = coursesAvailabilitiesFiltered[k].availability;
+              item.semester = coursesAvailabilitiesFiltered[k].semester;
+              availabilities.push(item);
+          }
+        }
+      }
+
+      $scope.availabilitiesBySemesters = availabilities;
+
       if(courses.status == 'unavailable'){
         $scope.showAvailabilityAlert('Course not available for ' + $scope.currentSemester,
           courses.course + ' is only available for the current ' +
-          'semesters : <br/> W16 - D, S16 - N, W17 - D, S17 - N');
+          'semesters : <br/><br/> W16(' + 'D' + ')  -  S16(' + 'N' + ')  -  W17(' + 'D' + ')  -  S17(' + 'N' + ')');
       }
     };
 
